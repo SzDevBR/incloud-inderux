138e88636a4d0a4011ae14f8721f91872d3f9296
SzDevBR <81070056+SzDevBR@users.noreply.github.com>
2023-08-13 15:20:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const express = require('express');
-const session = require('expression-session');
+const session = require('express-session');
 const passport = require('passport');
 const DiscordStrategy = require('passport-discord');
 const dotenv = require('dotenv');
